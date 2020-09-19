31589d9d7743c4d55d28c7abb534ff5c8d849698
MAXOUXAX <djmax486@gmail.com>
2020-09-19 12:38:08 +0200

    Added lessons and homeworks support

--- src/main/java/best/sti2d/therese/pronote/PronoteHelper.java
+++ src/main/java/best/sti2d/therese/pronote/PronoteHelper.java
@@ -2,6 +2,8 @@ package best.sti2d.therese.pronote;
 
 import best.sti2d.therese.Therese;
 import best.sti2d.therese.pronote.objects.Class;
+import best.sti2d.therese.pronote.objects.Homework;
+import best.sti2d.therese.pronote.objects.Lesson;
 import best.sti2d.therese.utils.EmbedCrafter;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.json.JSONArray;
@@ -11,6 +13,8 @@ import java.awt.*;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class PronoteHelper {
 
@@ -60,15 +64,69 @@ public class PronoteHelper {
         return messageEmbedList;
     }
 
-    public ArrayList<MessageEmbed> getHomeworksEmbed(String date) throws IOException {
+    public ArrayList<MessageEmbed> getHomeworksEmbeds(String date) throws IOException {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("query", "{homeworks(from: \""+date+"\") {description subject givenAt for color files}}");
+        jsonObject.put("query", "{homeworks(from: \""+date+"\") {description subject givenAt for color}}");
 
         JSONObject finalObject = pronoteManager.makeRequest("/graphql", jsonObject);
 
         System.out.println("finalObject.toString() = " + finalObject.toString());
+        JSONArray jsonArray = finalObject.getJSONObject("data").getJSONArray("homeworks");
         ArrayList<MessageEmbed> messageEmbedList = new ArrayList<>();
+        jsonArray.forEach(o -> {
+            JSONObject element = (JSONObject) o;
+            System.out.println("element.toString() = " + element.toString());
+            Homework currentHomework = new Homework(element);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM");
+
+            EmbedCrafter embedCrafter = new EmbedCrafter();
+            embedCrafter.setTitle(currentHomework.getSubject()+" - Pour le "+formatter.format(currentHomework.getDueTo()))
+                    .setDescription(currentHomework.getDescription()+"\n" +
+                            "**Donné le**: "+formatter.format(currentHomework.getGivenAt()))
+                    .setColor(currentHomework.getColor());
+            messageEmbedList.add(embedCrafter.build());
+        });
         return messageEmbedList;
     }
+
+    public ArrayList<MessageEmbed> getLessonsEmbeds(String date) throws IOException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("query", "{contents(from: \""+date+"\") {from to subject teachers color title description}}");
+
+        JSONObject finalObject = pronoteManager.makeRequest("/graphql", jsonObject);
+
+        System.out.println("finalObject.toString() = " + finalObject.toString());
+        JSONArray jsonArray = finalObject.getJSONObject("data").getJSONArray("contents");
+        ArrayList<MessageEmbed> messageEmbedList = new ArrayList<>();
+        jsonArray.forEach(o -> {
+            JSONObject element = (JSONObject) o;
+            System.out.println("element.toString() = " + element.toString());
+            Lesson currentLesson = new Lesson(element);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+
+            EmbedCrafter embedCrafter = new EmbedCrafter();
+            embedCrafter.setTitle(currentLesson.getSubject()+" - Cours du "+new SimpleDateFormat("dd/MM").format(currentLesson.getFrom()))
+                    .setDescription(currentLesson.getTitle()+"\n\n" +
+                            "**Contenu**: "+currentLesson.getDescription()+"\n" +
+                            "**Professeur(s)**: "+ listToString(Arrays.stream(currentLesson.getTeachers()).iterator())+"\n" +
+                            "**Horaires**: "+formatter.format(currentLesson.getFrom())+" » "+formatter.format(currentLesson.getTo())+"\n")
+                    .setColor(currentLesson.getColor());
+                    messageEmbedList.add(embedCrafter.build());
+        });
+        return messageEmbedList;
+    }
+
+    public String listToString(Iterator<String> iterator){
+        StringBuilder stringBuilder = new StringBuilder();
+        while(iterator.hasNext()){
+            String teacher = iterator.next();
+            stringBuilder.append(teacher);
+            if (iterator.hasNext())stringBuilder.append(", ");
+        }
+        return stringBuilder.toString();
+    }
+
 
 }
